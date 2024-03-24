efaa6b834f86580e1750b99c02e474bf7b853a41
Infamous-IT <gluhanuk98@gmail.com>
2024-03-24 23:45:20 +0200

    Helper

--- server/src/customer/service/customer.service.ts
+++ server/src/customer/service/customer.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import {CreateCustomerDto} from '../dto/create-customer.dto';
 import {UpdateCustomerDto} from '../dto/update-customer.dto';
+import { Customer } from '../entities/customer.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 
 @Injectable()
 export class CustomerService {
+  constructor(@InjectRepository(Customer) private readonly customerRepository: Repository<Customer>) {
+  }
+
   create(createCustomerDto: CreateCustomerDto) {
     return 'This action adds a new customer';
   }
